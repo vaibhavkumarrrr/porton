2a21cc98e5e13239fc9e19a074db0ce2bf30f3d5
vaibhavkumarrrr <156523525+vaibhavkumarrrr@users.noreply.github.com>
2024-01-31 19:29:32 +0530

    Create maing.py

---
+++ maing.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+EXP = [{
+    "company": "Career NJNUN Mantra",
+    "postion": "Social media marketing intern",
+    "location": "Remote",
+    "duration": 3
+}, {
+    "company": "Nex gen media",
+    "postion": "SMKJHGM",
+    "location": "Remote",
+    "duration": 1
+}, {
+    "company": "Nic",
+    "postion": "CNJOL,MNBGHreative marketing lead",
+    "location": "Bengaluru",
+    "duration ": 26
+}]
+
+
+@app.route('/')
+def index():
+  return render_template("home.html", expr=EXP, fname="vk")
+
+
+app.run(host='0.0.0.0', port=81, debug=True)
